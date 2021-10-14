26e974e01aeaed6d76226c1c16a87d77406427ff
wzh <1057549016@qq.com>
2021-10-14 15:32:29 +0800

    ss

---
+++ src/utils/index.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+export const isFalsy = (value) => (value === 0 ? false : !value);
+// 在一个函数中，改变传入的对象本身是不好的
+export const cleanObject = (object) => {
+  const result = { ...object };
+  Object.keys(result).forEach((key) => {
+    const value = result[key];
+    if (isFalsy(value)) {
+      delete result[key];
+    }
+  });
+  return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
+};
